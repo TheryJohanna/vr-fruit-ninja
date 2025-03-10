ffdb7c9c9faebf2b08eeab10d224d355454b8a08
Theresa Genßler <theresa.genssler@web.de>
2025-03-10 12:30:56 +0100

    Changed buttonscript to SendTo.Everyone

--- Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/ButtonPress.cs
+++ Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/ButtonPress.cs
@@ -31,7 +31,7 @@ public class ButtonPress : MonoBehaviour
         
     }
 
-    [Rpc(SendTo.Server)]
+    [Rpc(SendTo.Everyone)]
     void OnTriggerEnterRpc(Collider other)
     {
         if (!isPressed && other.CompareTag("User"))
@@ -43,7 +43,7 @@ public class ButtonPress : MonoBehaviour
         }
     }
 
-    [Rpc(SendTo.Server)]
+    [Rpc(SendTo.Everyone)]
     void OnTriggerExitRpc(Collider other)
     {
         if (other.gameObject == _pressingObject)

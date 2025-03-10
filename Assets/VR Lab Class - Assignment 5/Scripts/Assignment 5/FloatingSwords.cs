8ed94639aeb1e107d519c2cb5eaa903795d4f53d
Theresa Genßler <theresa.genssler@web.de>
2025-03-10 10:08:59 +0100

    changed placement of swords

--- Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/FloatingSwords.cs
+++ Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/FloatingSwords.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -8,32 +9,42 @@ public class FloatingSwords : MonoBehaviour
     private Rigidbody _rigidbody;
     private float _originalY;
     private float _randomness;
-    private bool _isGrabbed = false;
+    private NetworkVariable<bool> _isGrabbed = new(false);
     public float floatStrength = 0.05f;
     private XRGrabInteractable _grabInteractable;
 
-    void Start()
+    void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _grabInteractable = GetComponent<XRGrabInteractable>();
+    }
+    
+    void Start()
+    {
         _originalY = _rigidbody.position.y;
         _randomness = Random.Range(0f, Mathf.PI * 2f);
         
-        _grabInteractable = GetComponent<XRGrabInteractable>();
-        _grabInteractable.onSelectEntered.AddListener(OnGrab);
+        _grabInteractable.selectEntered.AddListener(OnGrab);
     }
 
     void FixedUpdate()
     {
-        if (!_isGrabbed)
+        if (!_isGrabbed.Value)
         {
             float floatOffset = Mathf.Sin(Time.time + _randomness) * floatStrength;
             _rigidbody.MovePosition(new Vector3(_rigidbody.position.x, _originalY + floatOffset,
                 _rigidbody.position.z));
         }
     }
-    
-    private void OnGrab(XRBaseInteractor interactor)
+
+    private void OnGrab(SelectEnterEventArgs args)
     {
-        _isGrabbed = true;
+        RequestGrabServerRpc();
+    }
+    
+    [Rpc(SendTo.Server)] 
+    private void RequestGrabServerRpc()
+    {
+        _isGrabbed.Value = true;
     }
 }

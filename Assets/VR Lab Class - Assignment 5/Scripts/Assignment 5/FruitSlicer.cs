1c694e502b1691853c8f3305fc026ab65575dfec
neo <chrissiguetter2001@gmail.com>
2025-03-11 15:53:19 +0100

    finally fully fix network slicing, add separate fruit slices

--- Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/FruitSlicer.cs
+++ Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/FruitSlicer.cs
@@ -8,7 +8,7 @@ using Unity.XR.CoreUtils;
 public class FruitSlicer : NetworkBehaviour
 {
     public float sliceForce = 5f;   // Force applied to sliced pieces
-    public GameObject fruitSlices;
+    public GameObject[] fruitSlices;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,16 +22,16 @@ public class FruitSlicer : NetworkBehaviour
     [Rpc(SendTo.Server)]
     private void SliceFruitRpc()
     {
-        var newSlice = Instantiate(fruitSlices, gameObject.transform.position, gameObject.transform.rotation);
-        var netObject = newSlice.GetComponent<NetworkObject>();
-        netObject.Spawn();
-        newSlice.SetLayerRecursively(8);
-        foreach (Transform child in newSlice.transform)
+        if (fruitSlices.Length == 2)
         {
-            var netChildObject = child.GetComponent<NetworkObject>();
-            netChildObject.Spawn();
-            var childRigidbody = child.gameObject.GetComponent<Rigidbody>();
-            childRigidbody.AddForce(new Vector3(UnityEngine.Random.Range(-1f, 1f), 0f, 0f) * sliceForce, ForceMode.Impulse);
+            foreach (var fruit in fruitSlices)
+            {
+                var newSlice = Instantiate(fruit, gameObject.transform.position, gameObject.transform.rotation);
+                var netObject = newSlice.GetComponent<NetworkObject>();
+                var rigidbody = newSlice.GetComponent<Rigidbody>();
+                netObject.Spawn();
+                rigidbody.AddForce(new Vector3(UnityEngine.Random.Range(-1f, 1f), 0f, 0f) * sliceForce, ForceMode.Impulse);
+            }
         }
         Destroy(gameObject); // Destroy original object
     }

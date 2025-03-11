634092934f94589fa60a69e77802790495aedb8f
Theresa Genßler <theresa.genssler@web.de>
2025-03-11 13:22:36 +0100

    Changed FloatingSwords.cs to maybe work now in the network

--- Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/FloatingSwords.cs
+++ Assets/VR Lab Class - Assignment 5/Scripts/Assignment 5/FloatingSwords.cs
@@ -1,15 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class FloatingSwords : MonoBehaviour
+public class FloatingSwords : NetworkBehaviour
 {
     private Rigidbody _rigidbody;
     private float _originalY;
     private float _randomness;
-    private NetworkVariable<bool> _isGrabbed = new(false);
+    private NetworkVariable<bool> _isGrabbed = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     public float floatStrength = 0.05f;
     private XRGrabInteractable _grabInteractable;
 
@@ -25,26 +23,47 @@ public class FloatingSwords : MonoBehaviour
         _randomness = Random.Range(0f, Mathf.PI * 2f);
         
         _grabInteractable.selectEntered.AddListener(OnGrab);
+        _grabInteractable.selectExited.AddListener(OnRelease);
     }
 
     void FixedUpdate()
     {
-        if (!_isGrabbed.Value)
+        if (IsOwner)
         {
-            float floatOffset = Mathf.Sin(Time.time + _randomness) * floatStrength;
-            _rigidbody.MovePosition(new Vector3(_rigidbody.position.x, _originalY + floatOffset,
-                _rigidbody.position.z));
+            if (!_isGrabbed.Value)
+            {
+                float floatOffset = Mathf.Sin(Time.time + _randomness) * floatStrength;
+                _rigidbody.MovePosition(new Vector3(_rigidbody.position.x, _originalY + floatOffset,
+                    _rigidbody.position.z));
+            }
         }
     }
 
     private void OnGrab(SelectEnterEventArgs args)
     {
-        RequestGrabRpc();
+        if (IsOwner)
+        {
+            RequestGrabRpc();
+        }
     }
-    
-    [Rpc(SendTo.Server)] 
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        if (IsOwner)
+        {
+            RequestReleaseRpc();
+        }
+    }
+
+    [Rpc(SendTo.Server)]
     private void RequestGrabRpc()
     {
         _isGrabbed.Value = true;
+    }
+
+    [Rpc(SendTo.Server)]
+    private void RequestReleaseRpc()
+    {
+        _isGrabbed.Value = false;
     }
 }
